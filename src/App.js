c5c1ddd11d0a958cc148b5ff809701f894d9975a
Isaiah-Ferguson <crestice@yahoo.com>
2023-03-24 14:44:01 -0700

    Peer Review Push

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+//Isaiah Keith Ferguson
+//3/24/2023
+//Weather App
+//Peer Reviewed by Jacob Dekok: This project is great. Isaiah went all out, with all the bells and whistles. I love the sliding text across the top, the literal day and night mode, and the icons for the different weather. The code is all very organized and runs great. He did a really great job with this project.
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import WeatherDashboard from './components/WeatherDashboard';
